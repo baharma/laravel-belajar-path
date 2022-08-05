0dcf89b17ec25c32b3890d6421ce46f9f30a9d64
baharma <bagusaditya1899@gmail.com>
2022-08-05 23:11:37 +0800

    model

---
+++ crud-foto-relasi-2/app/Http/Controllers/PembuatController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Pembuat;
+use Illuminate\Http\Request;
+
+class PembuatController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Pembuat  $pembuat
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Pembuat $pembuat)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Pembuat  $pembuat
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Pembuat $pembuat)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Pembuat  $pembuat
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Pembuat $pembuat)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Pembuat  $pembuat
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Pembuat $pembuat)
+    {
+        //
+    }
+}
