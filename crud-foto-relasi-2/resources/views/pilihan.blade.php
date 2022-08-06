01c02df938fdd2093d6ea0a729397e3b96d95b90
baharma <bagusaditya1899@gmail.com>
2022-08-06 18:50:08 +0800

    test

---
+++ crud-foto-relasi-2/resources/views/pilihan.blade.php
@@ -0,0 +1,19 @@
+@extends('layout.app')
+@section('content')
+<div class="d-flex justify-content-center">
+    <div class="card m-4" style="width: 18rem;">
+        <div class="card-body">
+          <h5 class="card-title">Pembuat</h5>
+          <p class="card-text">Memasukan data Pembuat buku tersebut</p>
+          <a href="#" class="btn btn-primary">Go Pembuat</a>
+        </div>
+      </div>
+      <div class="card m-4" style="width: 18rem;">
+        <div class="card-body">
+          <h5 class="card-title">Buku</h5>
+          <p class="card-text">Membuat list buku pastikan membuat nama pembuat dahulu.</p>
+          <a href="#" class="btn btn-primary">Go Buku</a>
+        </div>
+      </div>
+</div>
+@endsection
